ac463c2789c63c424e9a14f43c2c79eb20520452
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2018-10-30 06:57:55 -0700

    removed README

--- src/test/java/postfix/PostfixEvaluatorTest.java
+++ src/test/java/postfix/PostfixEvaluatorTest.java
@@ -6,14 +6,29 @@ import org.junit.Test;
 
 public class PostfixEvaluatorTest {
 
-	// Implement a sufficient number of tests to have
-	// reasonable confidence in the correctness of your
-	// PostfixEvaluator implementation.
-	
 	@Test
 	public void test1() throws MalformedExpressionException {
-	    PostfixEvaluator pe = new PostfixEvaluator("5 2 +");
-	    pe.eval();
+	    assertEquals(7, new PostfixEvaluator("5 2 +").eval(), 0);
 	}
+
+    @Test
+    public void test2() throws MalformedExpressionException {
+        assertEquals(8, new PostfixEvaluator("5 2 + 7 - 8 +").eval(), 0);
+    }
+
+    @Test
+    public void test3() throws MalformedExpressionException {
+        assertEquals(-40, new PostfixEvaluator("5 2 + 7 - 8 + 0 5 - *").eval(), 0);
+    }
+
+    @Test(expected = MalformedExpressionException.class)
+    public void test4() throws MalformedExpressionException {
+        assertEquals(-40, new PostfixEvaluator("5 2 + 7 - 8 + 0 5 - * +").eval(), 0);
+    }
+
+    @Test(expected = MalformedExpressionException.class)
+    public void test5() throws MalformedExpressionException {
+        new PostfixEvaluator("+").eval();
+    }
 
 }
